a6697c1f68f5731fea04465638fd6269ce547c17
willo\william weiler <w.weiler@ludus-academie.com>
2017-06-05 13:37:09 +0200

    no message

--- bull_and_cow.dfm
+++ bull_and_cow.dfm
@@ -14,13 +14,105 @@ object Form1: TForm1
   OnCreate = FormCreate
   PixelsPerInch = 96
   TextHeight = 13
+  object Label1: TLabel
+    Left = 419
+    Top = 117
+    Width = 21
+    Height = 13
+    Caption = 'Cow'
+  end
+  object Label2: TLabel
+    Left = 103
+    Top = 117
+    Width = 16
+    Height = 13
+    Caption = 'Bull'
+  end
+  object Label3: TLabel
+    Left = 103
+    Top = 29
+    Width = 27
+    Height = 13
+    Caption = 'Saisie'
+  end
+  object Label4: TLabel
+    Left = 432
+    Top = 21
+    Width = 42
+    Height = 13
+    Caption = 'Essai/Vie'
+  end
   object Memo1: TMemo
     Left = 0
     Top = 0
-    Width = 185
-    Height = 89
+    Width = 97
+    Height = 561
     Lines.Strings = (
       'Memo1')
     TabOrder = 0
+    OnChange = Memo1Change
+  end
+  object Esaisie: TEdit
+    Left = 103
+    Top = 48
+    Width = 121
+    Height = 21
+    TabOrder = 1
+  end
+  object Ebull: TEdit
+    Left = 103
+    Top = 136
+    Width = 121
+    Height = 21
+    TabOrder = 2
+  end
+  object Ecow: TEdit
+    Left = 419
+    Top = 136
+    Width = 121
+    Height = 21
+    TabOrder = 3
+  end
+  object Bcheck: TButton
+    Left = 256
+    Top = 88
+    Width = 75
+    Height = 25
+    Caption = 'CHECK'
+    TabOrder = 4
+    OnClick = BcheckClick
+  end
+  object Braz: TButton
+    Left = 120
+    Top = 272
+    Width = 75
+    Height = 25
+    Caption = 'R.A.Z'
+    TabOrder = 5
+    OnClick = BrazClick
+  end
+  object Bquit: TButton
+    Left = 432
+    Top = 272
+    Width = 75
+    Height = 25
+    Caption = 'QUITTER'
+    TabOrder = 6
+    OnClick = BquitClick
+  end
+  object mot: TEdit
+    Left = 103
+    Top = 2
+    Width = 121
+    Height = 21
+    TabOrder = 7
+    OnChange = motChange
+  end
+  object casevie: TEdit
+    Left = 423
+    Top = 48
+    Width = 51
+    Height = 21
+    TabOrder = 8
   end
 end
--- bull_and_cow.pas
+++ bull_and_cow.pas
@@ -6,10 +6,30 @@ uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
 
+const
+maxmot=10;
+
 type
   TForm1 = class(TForm)
     Memo1: TMemo;
+    Esaisie: TEdit;
+    Ebull: TEdit;
+    Ecow: TEdit;
+    Bcheck: TButton;
+    Braz: TButton;
+    Bquit: TButton;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    mot: TEdit;
+    casevie: TEdit;
     procedure FormCreate(Sender: TObject);
+    procedure BquitClick(Sender: TObject);
+    procedure BrazClick(Sender: TObject);
+    procedure BcheckClick(Sender: TObject);
+    procedure motChange(Sender: TObject);
+    procedure Memo1Change(Sender: TObject);
   private
     { Déclarations privées }
   public
@@ -18,14 +38,104 @@ type
 
 var
   Form1: TForm1;
-
-implementation
+  nbbull:integer;
+  nbcow:integer;
+  nbvie:integer;
+  saisiemot,motjuste:string;
+  implementation
 
 {$R *.dfm}
+procedure vie(motjuste:string; var nbvie:integer); //vie donner en fonction des lettres du mot
+begin
+case length(motjuste) of
+3: nbvie:=4;
+4: nbvie:=7;
+5: nbvie:=10;
+6: nbvie:=16;
+7: nbvie:=20;
+
+end;
+end;
+procedure verife(var nbbull:integer; var nbcow:integer; saisiemot,motjuste:string);
+var
+i,j:integer;
+testlettre:boolean;
+testbull:boolean;
+begin
+nbbull:=0;
+nbcow:=0;
+
+for i:=1 to length(saisiemot)  do
+  begin
+  for j := 1 to length(motjuste) do
+    begin
+     testlettre:=False;
+     testbull:=false;
+    if (copy(saisiemot,i,1)=copy(motjuste,j,1))  then
+      begin
+        testlettre:=true;
+      end;
+    if (testlettre=true) and(i=j) then
+    begin
+    nbbull:=nbbull+1;
+    testbull:=true;
+    end;
+    if (testlettre=true) and (testbull=false) then
+      nbcow:=nbcow+1;
+
+    end;
+   end;
+end;
+
+procedure TForm1.BcheckClick(Sender: TObject);
+begin
+//permet de bloqué en miniscule
+saisiemot:=lowercase(Esaisie.Text);
+verife(nbbull,nbcow,saisiemot,motjuste);
+Ebull.Text:=inttostr(nbbull);
+Ecow.Text:=inttostr(nbcow);
+if Esaisie.Text=mot.Text then
+showmessage('bravo vous avez gagner')
+else
+  begin
+    nbvie:=nbvie-1;
+    casevie.Text:=inttostr(nbvie);
+  end;
+  if nbvie=0 then
+  showmessage('LOOSER tu as perdu');
+end;
+
+procedure TForm1.BquitClick(Sender: TObject);
+begin
+close;
+end;
+
+procedure TForm1.BrazClick(Sender: TObject);
+begin
+form1.Esaisie.Text:=' ';
+form1.Ebull.Text:= ' ';
+form1.Ecow.Text:=' ';
+
+end;
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
-memo1.lines.loadfromfile('C:\Users\william weiler\Desktop\cours\Devoir_15_05_2017');
+
+memo1.lines.loadfromfile('C:\Users\william weiler\Desktop\cours\Devoir_15_05_2017\liste_mot.txt');
+motjuste:=memo1.Lines[random(maxmot+1)];
+mot.Text:=motjuste;
+vie(motjuste,nbvie);
+casevie.Text:=inttostr(nbvie);
+end;
+
+procedure TForm1.Memo1Change(Sender: TObject);
+begin
+memo1.visible:=false;
+end;
+
+procedure TForm1.motChange(Sender: TObject);
+begin
+mot.visible:=false;
 end;
 
 end.
